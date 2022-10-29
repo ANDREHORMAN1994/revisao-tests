ad8875932cf9d751c8612417e21fa4ea23897181
Raypcte <rayanevalak@gmail.com>
2022-10-29 19:52:27 -0300

    iniciando testes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import { Button, Card, CardActions,
-  CardContent, CardMedia, Grid, Stack, Typography } from '@mui/material';
+import {
+  Autocomplete,
+  Backdrop,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useCallback, useEffect, useState } from 'react';
 import PetsIcon from '@mui/icons-material/Pets';
 
@@ -7,8 +18,8 @@ const endpointDog = 'https://dog.ceo/api/breeds/image/random';
 const endpointCat = 'https://api.thecatapi.com/v1/images/search';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [animal, setAnimal] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [animalImg, setAnimalImg] = useState('');
 
   const requestApi = async (endpoint) => {
     const response = await fetch(endpoint);
@@ -16,65 +27,93 @@ function App() {
     return data;
   };
 
-  const handleAnimal = useCallback(async (endpoint) => {
+  const handleAnimal = useCallback(async (value) => {
     let imgUrl = '';
+    const endpoint = value.includes('Dog') ? endpointDog : endpointCat;
     setLoading(true);
     const data = await requestApi(endpoint);
     imgUrl = data?.status === 'success'
-      ? { name: 'Dog', image: data.message }
-      : { name: 'Cat', image: imgUrl = data[0].url };
-    setAnimal(imgUrl);
+      ? { name: 'DOG', image: data.message }
+      : { name: 'CAT', image: (imgUrl = data[0].url) };
+    setAnimalImg(imgUrl);
     setLoading(false);
+    global.alert('successful request!');
   }, []);
 
   useEffect(() => {
     handleAnimal(endpointDog);
   }, [handleAnimal]);
 
-  if (loading) return <h1>Loading...</h1>;
-
-  return (
-    <Grid container spacing={ 2 } sx={ { height: '100vh' } }>
-      <Grid item xs={ 12 } sx={ { margin: 'auto' } }>
-        <Card sx={ { maxWidth: 500, margin: 'auto' } }>
-          <CardMedia
-            component="img"
-            height="400"
-            image={ animal.image }
-            alt="animal"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {animal.name}
-            </Typography>
-            {/* <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
-          </Typography> */}
-          </CardContent>
-          <CardActions>
-            <Stack direction="row" spacing={ 2 }>
-              <Button
-                variant="contained"
-                color="error"
-                startIcon={ <PetsIcon /> }
-                onClick={ () => handleAnimal(endpointDog) }
+  return loading ? (
+    <Backdrop
+      sx={ { color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 } }
+      open={ loading }
+    >
+      <CircularProgress color="inherit" />
+      <h1>Loading...</h1>
+    </Backdrop>
+  ) : (
+    <div>
+      <Typography
+        gutterBottom
+        variant="h2"
+        component="header"
+        align="center"
+        sx={ { background: animalImg.name === 'DOG' ? '#0088d1' : '#2f7c31',
+          color: 'white',
+          margin: 0,
+          fontWeight: 600 } }
+      >
+        DOGS VS CATS
+      </Typography>
+      <Grid container sx={ { height: '100vh', background: '#e5e5e5' } }>
+        <Grid item xs={ 12 } sx={ { margin: 'auto' } }>
+          <Card sx={ { maxWidth: 500, margin: 'auto' } }>
+            <CardMedia
+              component="img"
+              height="400"
+              image={ animalImg.image }
+              alt="animalImg"
+            />
+            <CardContent>
+              <Stack direction="row" spacing={ 2 } justifyContent="center">
+                <Typography
+                  gutterBottom
+                  variant="h5"
+                  component="p"
+                  align="center"
+                  fontWeight="600"
+                >
+                  {animalImg.name}
+                </Typography>
+                <PetsIcon
+                  color={ animalImg.name === 'DOG' ? 'info' : 'success' }
+                />
+              </Stack>
+            </CardContent>
+            <CardActions>
+              <Stack
+                direction="row"
+                justifyContent="center"
+                width="100vw"
+                paddingBottom={ 5 }
               >
-                DOGS
-              </Button>
-              <Button
-                variant="contained"
-                color="success"
-                endIcon={ <PetsIcon /> }
-                onClick={ () => handleAnimal(endpointCat) }
-              >
-                CATS
-              </Button>
-            </Stack>
-          </CardActions>
-        </Card>
+                <Autocomplete
+                  disablePortal
+                  id="select-animals"
+                  options={ ['Cat', 'Dog'] }
+                  onChange={ (_, value) => handleAnimal(value) }
+                  sx={ { width: 300 } }
+                  renderInput={ (params) => (
+                    <TextField { ...params } label="Dog or Cat" />
+                  ) }
+                />
+              </Stack>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,46 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, waitForElementToBeRemoved } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testando aplicacao', () => {
+  // it.todo('verificar elementos na tela');
+  it('verificar elementos na tela', async () => {
+    // jest.spyOn(global, 'alert')
+    //   .mockImplementation(() => 'teste');
+
+    global.alert = jest.fn(() => 'teste');
+    render(<App />);
+
+    // GET BY
+    const loadingEl = screen.getByText('loading', { exact: false });
+    expect(loadingEl).toBeInTheDocument();
+
+    await waitForElementToBeRemoved(loadingEl);
+
+    expect(alert).toHaveBeenCalledTimes(1);
+
+    // FIND BY
+    // const titleEl = await screen.findByText('DOGS VS CATS');
+    // expect(titleEl).toBeInTheDocument();
+
+    // WAIT FOR
+    // await waitFor(() => expect(screen.getByText('DOGS VS CATS')).toBeInTheDocument());
+
+    screen.getByText('DOGS VS CATS');
+  });
+
+  it.todo('verificar elementos assíncronos');
 });
+
+// - FUNÇÃO AUXILIAR renderWithRouter e Provider
+
+// - MOCK API - CASO DE SUCESSO;
+
+// - MOCK API - CASO DE ERROR;
+
+// - MOCK ALERTA;
+
+// - TESTE ASSINCRONO (find, waitFor ...);
+
+// - ESPERAR ELEMENTOS APARECER NA TELA
+
+// - TESTAR FORMS (INPUTS E SELECTS)
