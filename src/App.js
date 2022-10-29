62491031e299254b09391674c98631f7e6a84bf0
ANDREHORMAN1994 <andre.horman1994@hotmail.com>
2022-10-29 13:34:55 -0300

    :construction: feat: fix exercise

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Button, Card, CardActions,
-  CardContent, CardMedia, Stack, Typography } from '@mui/material';
+  CardContent, CardMedia, Grid, Stack, Typography } from '@mui/material';
 import { useCallback, useEffect, useState } from 'react';
 import PetsIcon from '@mui/icons-material/Pets';
 
@@ -34,46 +34,47 @@ function App() {
   if (loading) return <h1>Loading...</h1>;
 
   return (
-    <div className="App">
-      {/* <img src={ animal } alt="animal" width="500" /> */}
-      <Card sx={ { maxWidth: 500 } }>
-        <CardMedia
-          component="img"
-          height="400"
-          image={ animal.image }
-          alt="animal"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {animal.name}
-          </Typography>
-          {/* <Typography variant="body2" color="text.secondary">
+    <Grid container spacing={ 2 } sx={ { height: '100vh' } }>
+      <Grid item xs={ 12 } sx={ { margin: 'auto' } }>
+        <Card sx={ { maxWidth: 500, margin: 'auto' } }>
+          <CardMedia
+            component="img"
+            height="400"
+            image={ animal.image }
+            alt="animal"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {animal.name}
+            </Typography>
+            {/* <Typography variant="body2" color="text.secondary">
             Lizards are a widespread group of squamate reptiles, with over 6,000
             species, ranging across all continents except Antarctica
           </Typography> */}
-        </CardContent>
-        <CardActions>
-          <Stack direction="row" spacing={ 2 }>
-            <Button
-              variant="contained"
-              color="error"
-              startIcon={ <PetsIcon /> }
-              onClick={ () => handleAnimal(endpointDog) }
-            >
-              DOGS
-            </Button>
-            <Button
-              variant="contained"
-              color="success"
-              endIcon={ <PetsIcon /> }
-              onClick={ () => handleAnimal(endpointCat) }
-            >
-              CATS
-            </Button>
-          </Stack>
-        </CardActions>
-      </Card>
-    </div>
+          </CardContent>
+          <CardActions>
+            <Stack direction="row" spacing={ 2 }>
+              <Button
+                variant="contained"
+                color="error"
+                startIcon={ <PetsIcon /> }
+                onClick={ () => handleAnimal(endpointDog) }
+              >
+                DOGS
+              </Button>
+              <Button
+                variant="contained"
+                color="success"
+                endIcon={ <PetsIcon /> }
+                onClick={ () => handleAnimal(endpointCat) }
+              >
+                CATS
+              </Button>
+            </Stack>
+          </CardActions>
+        </Card>
+      </Grid>
+    </Grid>
   );
 }
 
